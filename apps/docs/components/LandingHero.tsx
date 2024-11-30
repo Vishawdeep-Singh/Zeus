44f81c60f8df4cbab747d21dc1ebe58f2498f08a
Rajnish2105 <166371359+Rajnish2105@users.noreply.github.com>
2024-11-30 21:52:21 +0530

    basics of landing page

---
+++ apps/docs/components/LandingHero.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "./ui/container-scroll-animation";
+import Image from "next/image";
+
+export function LandingHero() {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-black dark:text-white">
+              Unleash the power of <br />
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                The God Inside You
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <Image
+          src={'https://images.unsplash.com/photo-1519859660545-3dea8ddf683c?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
