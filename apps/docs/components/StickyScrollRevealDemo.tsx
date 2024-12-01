e8c0b0611df1449525a5978ed32527c8f1611a72
Rajnish2105 <166371359+Rajnish2105@users.noreply.github.com>
2024-12-01 17:49:37 +0530

    fixed landing page

--- apps/docs/components/StickyScrollRevealDemo.tsx
+++ apps/docs/components/StickyScrollRevealDemo.tsx
@@ -1,179 +1,179 @@
-"use client";
-import React from "react";
-import { StickyScroll } from "./ui/sticky-scroll-reveal";
-import Image from "next/image";
+'use client';
+import React from 'react';
+import { StickyScroll } from './ui/sticky-scroll-reveal';
+import Image from 'next/image';
 
 const content = [
   {
-    title: "Comprehensive Analytics at Your Fingertips",
+    title: 'Comprehensive Analytics at Your Fingertips',
     description:
       "Gain insights into your gym's performance with detailed analytics. View attendance data for the last 7 days and revenue trends over the past 6 months. Easily filter results to track multiple gym locations separately for better decision-making.",
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll01.png'}
-          width={1000}
-          height={1000}
-          className="h-full w-full object-cover"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Stay Ahead of Membership Expirations",
+    title: 'Stay Ahead of Membership Expirations',
     description:
       "Keep track of expiring memberships effortlessly. When a user's membership nears its end, they’re added to the membership expiry table for timely reminders, ensuring no one misses out on renewing.",
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll02.png'}
-          width={1000}
-          height={1000}
-          className="h-full w-full object-cover"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Real-Time Attendance Tracking",
+    title: 'Real-Time Attendance Tracking',
     description:
-      "Get live updates on your gym’s attendance. See who has visited, track member check-ins, and stay on top of your gym’s activity to ensure smooth operations.",
+      'Get live updates on your gym’s attendance. See who has visited, track member check-ins, and stay on top of your gym’s activity to ensure smooth operations.',
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll3.png'}
-          width={1000}
-          height={1000}
-          className="h-full w-full object-cover"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Effortless Member Search with Advanced Filters",
+    title: 'Effortless Member Search with Advanced Filters',
     description:
-      "Quickly find members using a powerful search tool with filters like date, gym name, and user name. View attendance records for specific dates or ranges, ensuring accurate tracking of gym activity.",
+      'Quickly find members using a powerful search tool with filters like date, gym name, and user name. View attendance records for specific dates or ranges, ensuring accurate tracking of gym activity.',
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll4.png'}
-          width={2000}
-          height={2000}
-          className="h-full w-full object-fill"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Simplified Gym Management",
+    title: 'Simplified Gym Management',
     description:
-      "Easily manage all of your gyms in one place. View and control key information for each location, ensuring smooth operations across your gym empire.",
+      'Easily manage all of your gyms in one place. View and control key information for each location, ensuring smooth operations across your gym empire.',
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll5.png'}
-          width={2000}
-          height={2000}
-          className="h-full w-full object-fill"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Seamless Membership Management",
+    title: 'Seamless Membership Management',
     description:
       "Effortlessly add new memberships to any of your gyms. Within each gym, you can manage memberships directly, giving you full control over your gym's membership base.",
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll6.png'}
-          width={2000}
-          height={2000}
-          className="h-full w-full object-fill"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Member Profiles & Insights",
+    title: 'Member Profiles & Insights',
     description:
-      "Gain a deeper understanding of your gym members. View detailed stats and information for each member, making it easy to track their engagement and manage memberships efficiently.",
+      'Gain a deeper understanding of your gym members. View detailed stats and information for each member, making it easy to track their engagement and manage memberships efficiently.',
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll7.png'}
-          width={2000}
-          height={2000}
-          className="h-full w-full object-fill"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "User-Friendly Interface for Members",
+    title: 'User-Friendly Interface for Members',
     description:
-      "A clean, intuitive user interface for your gym members. Users can easily join gyms, track memberships, and engage with your gym’s services from their own dashboard.",
+      'A clean, intuitive user interface for your gym members. Users can easily join gyms, track memberships, and engage with your gym’s services from their own dashboard.',
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll8.png'}
-          width={2000}
-          height={2000}
-          className="h-full w-full object-fill"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Monitor Your Gym Memberships",
+    title: 'Monitor Your Gym Memberships',
     description:
-      "Users can track their memberships across all gyms. With clear indicators of days left until expiry, users are notified of upcoming renewals, so they never miss a beat.",
+      'Users can track their memberships across all gyms. With clear indicators of days left until expiry, users are notified of upcoming renewals, so they never miss a beat.',
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll9.png'}
-          width={2000}
-          height={2000}
-          className="h-full w-full object-fill"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Effortless Attendance Tracking for Members",
+    title: 'Effortless Attendance Tracking for Members',
     description:
-      "Users can quickly mark their attendance at joined gyms. Gym admins and owners receive notifications in real time, ensuring accurate tracking of attendance for all members.",
+      'Users can quickly mark their attendance at joined gyms. Gym admins and owners receive notifications in real time, ensuring accurate tracking of attendance for all members.',
     content: (
       <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src={'/scroll10.png'}
-          width={2000}
-          height={2000}
-          className="h-full w-full object-fill"
+          fill
+          priority
+          className="h-full w-full object-contain"
           alt="linear board demo"
         />
       </div>
     ),
   },
   {
-    title: "Advanced Search for Members & Attendance",
+    title: 'Advanced Search for Members & Attendance',
     description:
-      "Utilize a robust search engine to find specific member attendance data. With multiple filters, including date, gym name, and user, you can access tailored results for precise record-keeping.",
+      'Utilize a robust search engine to find specific member attendance data. With multiple filters, including date, gym name, and user, you can access tailored results for precise record-keeping.',
     content: (
-      <div className="h-full w-full flex items-center justify-center text-white">
+      <div className="h-full w-full flex items-center justify-center text-white object-contain">
         <Image
           src={'/scroll4.png'}
-          width={1000}
-          height={1000}
+          fill
+          priority
           className="h-full w-full object-contain"
           alt="linear board demo"
         />
--- apps/docs/components/ui/sticky-scroll-reveal.tsx
+++ apps/docs/components/ui/sticky-scroll-reveal.tsx
@@ -1,8 +1,8 @@
-"use client";
-import React, { useEffect, useRef, useState } from "react";
-import { useMotionValueEvent, useScroll } from "framer-motion";
-import { motion } from "framer-motion";
-import { cn } from "@/lib/utils";
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+import { useMotionValueEvent, useScroll } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { cn } from '@/lib/utils';
 
 export const StickyScroll = ({
   content,
@@ -21,11 +21,11 @@ export const StickyScroll = ({
     // uncomment line 22 and comment line 23 if you DONT want the overflow container and want to have it change on the entire page scroll
     // target: ref
     container: ref,
-    offset: ["start start", "end start"],
+    offset: ['start start', 'end start'],
   });
   const cardLength = content.length;
 
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
     const cardsBreakpoints = content.map((_, index) => index / cardLength);
     const closestBreakpointIndex = cardsBreakpoints.reduce(
       (acc, breakpoint, index) => {
@@ -42,14 +42,14 @@ export const StickyScroll = ({
   });
 
   const backgroundColors = [
-    "var(--slate-900)",
-    "var(--black)",
-    "var(--neutral-900)",
+    'var(--slate-900)',
+    'var(--black)',
+    'var(--neutral-900)',
   ];
   const linearGradients = [
-    "linear-gradient(to bottom right, var(--cyan-500), var(--emerald-500))",
-    "linear-gradient(to bottom right, var(--pink-500), var(--indigo-500))",
-    "linear-gradient(to bottom right, var(--orange-500), var(--yellow-500))",
+    'linear-gradient(to bottom right, var(--cyan-500), var(--emerald-500))',
+    'linear-gradient(to bottom right, var(--pink-500), var(--indigo-500))',
+    'linear-gradient(to bottom right, var(--orange-500), var(--yellow-500))',
   ];
 
   const [backgroundGradient, setBackgroundGradient] = useState(
@@ -104,9 +104,8 @@ export const StickyScroll = ({
         </div>
       </div>
       <div
-        style={{ background: backgroundGradient }}
         className={cn(
-          "hidden lg:block h-full w-full rounded-md bg-white sticky top-3 overflow-hidden",
+          'hidden lg:block h-auto w-[60%] bg-white rounded-md sticky top-3 overflow-hidden',
           contentClassName
         )}
       >
